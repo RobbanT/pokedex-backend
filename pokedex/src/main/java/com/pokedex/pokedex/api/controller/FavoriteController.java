5f0c150b4590292cb2b9d40e6075e2d0687d6926
Roberto Tencic <roberto.tencic@outlook.com>
2024-02-01 20:32:25 +0100

    fix

--- pokedex/src/main/java/com/pokedex/pokedex/api/controller/FavoriteController.java
+++ pokedex/src/main/java/com/pokedex/pokedex/api/controller/FavoriteController.java
@@ -13,26 +13,25 @@ public class FavoriteController {
         this.favoriteService = favoriteService;
     }
 
-    @GetMapping("/favorite")
-    public Favorite getfavorite(@RequestParam int id) {
-        return favoriteService.getFavorite(id);
-    }
-
+    @CrossOrigin(origins = "http://127.0.0.1:5500")
     @GetMapping("/favorites")
     public List<Favorite> getfavorites() {
         return favoriteService.getFavorites();
     }
 
+    @CrossOrigin(origins = "http://127.0.0.1:5500")
     @PostMapping("/favorite")
     public Favorite createFavorite(@RequestBody Favorite favorite) {
         return favoriteService.createFavorite(favorite.getId(), favorite.getNote());
     }
 
+    @CrossOrigin(origins = "http://127.0.0.1:5500")
     @PatchMapping("/favorite")
     public Favorite editFavorite(@RequestBody Favorite favorite) {
         return favoriteService.editFavorite(favorite.getId(), favorite.getNote());
     }
 
+    @CrossOrigin(origins = "http://127.0.0.1:5500")
     @DeleteMapping("/favorite")
     public Favorite deleteFavorite(@RequestParam int id) {
         return favoriteService.deleteFavorite(id);
